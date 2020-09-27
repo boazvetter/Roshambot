4d4447e17934e4f3b24fe272f877e8c5940b295a
Boaz Vetter <boazmvetter@gmail.com>
2020-09-27 22:23:54 +0200

    Add robohand moving

--- game.py
+++ game.py
@@ -8,14 +8,18 @@ VU Amsterdam 2020
 import numpy as np
 import cv2
 
+import serial
+import time
 import cv2
 import numpy as np
 import tensorflow as tf
 import keras
-
 from keras.utils import to_categorical
 from keras.utils.generic_utils import CustomObjectScope
 
+
+# Initialize serial connection with arduino and set baud rate
+arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)
 
 # Load model
 with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,
@@ -23,6 +27,7 @@ with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,
     model = tf.keras.models.load_model('rock-paper-scissors-model.h5',
     	custom_objects={'relu6': keras.applications.mobilenet.relu6})
 
+previousTime = time.perf_counter()
 
 
 def to_string(prediction):
@@ -36,9 +41,21 @@ def to_string(prediction):
 		return 'none'
 	pass
 
+def move_arm(prediction):
+    print('Prediction: ', prediction)
+    print(type(prediction))
+    if prediction == 0:
+        arduino.write("r".encode('utf-8'))
+    elif prediction == 1:
+        arduino.write("p".encode('utf-8'))
+    elif prediction == 2:
+        arduino.write("s".encode('utf-8'))
+    pass    
+
 
 # TODO Show webcam feed, overlay prediction
 cap = cv2.VideoCapture(0)
+ready_to_move = False
 
 while(True):
     # Capture frame-by-frame
@@ -66,13 +83,22 @@ while(True):
     cv2.putText(frame, predicted_move, (int(width*0.25),int(height*0.20)), 
     	font, 1.2, (255,255,255), 2, cv2.LINE_AA)
     cv2.imshow('Roshambot',frame)
+
+    # Move the robot arm
+    if(time.perf_counter() > (previousTime+3)):
+        if (np.argmax(prediction) != 3):
+            print("################################## Moving arm #############################")
+            move_arm(np.argmax(prediction))
+            previousTime = time.perf_counter()
+
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
-
-
 cap.release()
 cv2.destroyAllWindows()
+
+
+
 
 
 # TODO implement robot RPS strategy
